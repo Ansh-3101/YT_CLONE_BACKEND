43e327eeb994e99ca4bc1d7dd2554bff275baabc
Ansh-3101 <apsinghofficial3101@gmail.com>
2024-03-09 22:45:31 +0530

    Final commit

--- db.js
+++ db.js
@@ -1,15 +1,17 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-    process.env.DATABASE_NAME,
-    process.env.DATABASE_USERNAME,
-    process.env.DATABASE_PASSWORD,
-    {
-        host: process.env.DATABASE_HOST,
-        dialect: "mysql",
-        logging: (log) => console.log(log),
-    }
-);
+// const sequelize = new Sequelize(
+//     process.env.DATABASE_NAME,
+//     process.env.DATABASE_USERNAME,
+//     process.env.DATABASE_PASSWORD,
+//     {
+//         host: process.env.DATABASE_HOST,
+//         dialect: "mysql",
+//         logging: (log) => console.log(log),
+//     }
+// );
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {});
 
 console.log(sequelize);
 
