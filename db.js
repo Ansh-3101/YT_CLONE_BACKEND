f3505d3f8d55eb1632715adf9bbd7663f9ee071a
Ansh-3101 <112927849+Ansh-3101@users.noreply.github.com>
2024-03-09 23:32:18 +0530

    Update db.js

--- db.js
+++ db.js
@@ -11,7 +11,9 @@ const { Sequelize } = require("sequelize");
 //     }
 // );
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {});
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  logging:false
+});
 
 console.log(sequelize);
 
